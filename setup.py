73af8dba1f5e0e8f2ae586d9b298f2679c2c8fc9
Marek Paulik <marek.paulik@blindspot.ai>
2023-01-12 11:33:58 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Summarizer of youtube podcast videos using OpenAI Whisper and transformers',
+    author='Marek Paulik, David Kubanda',
+    license='',
+)
